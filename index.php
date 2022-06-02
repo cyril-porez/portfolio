815d2cb870a3e24f0d67d00434160ab5af12001a
cyril1989 <68592507+cyril1989@users.noreply.github.com>
2022-06-02 19:44:24 +0200

    en cours

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Cyril Porez</title>
+</head>
+<body>
+	<header>
+		<nav>
+			<div>
+				<a href="#">Porez Cyril</a>
+				<ul>
+					<a href="#a-propos"><li>About</li></a>
+					<a href="#competences"><li>Skills</li></a>
+					<a href="#projet"><li>Project</li></a>
+					<a href="#contact"><li>Contact</li></a>
+				</ul>
+			</div>
+		</nav>
+	</header>
+	
+	<main>
+		<section>
+			<img src="images/portofiio_background_img.png" alt="image d'accueil">
+			<div>
+				<p>Bonjour, je suis</p>
+				<p>Porez Cyril</p>
+				<p>Etudiant en devellopement <span>web & mobile</span></p>
+				<a href="#a-propos">A propos</a>
+			</div>
+			
+		</section>
+
+		<section id="a-propos">
+			
+			<img src="images/Jotaro.png" alt="ken">
+			<article>
+				<div>
+					<h1><span>A</span> propos :</h1>
+				</div>
+				
+				<div>
+					<p>Je m'appelle <span>Cyril Porez</span>, 32ans</p>
+					
+					<p>Diplomé d'un BTS Electronique, A la suite de mes expériences professionnelles, 
+					   j'ai décidé de m'orienter vers le développement Web.</p>
+					
+					<p>Je suis passionné par le milieu de la tech dans son ensemble, et j'ai eu la chance 
+					d'intégrer <span>La Plateforme_</span> pour me lancer dans une formation avec un apprentissage par projet.
+					Avec une forte mise en avant du travail d'équipe en utilisant la méthode agile</p>
+					
+					<p>Je recherche une <span>alternance</span> pour le mois de septembre 2022. Afin de mettre à 
+					l'épreuve les compétences acquise au cours de cette première année. Ambitieux et aimant le défi,
+					je serais ravis de mettre à votre disposition mon expérience.</p>
+				</div>
+			</article>			
+		</section>
+
+		<section id="competences">			
+			<article>
+				<div>
+					<h1><span>Com</span>pétences :</h1>
+				</div>
+
+				<section>
+					<div>
+						<!--<img src="images/PHP-logo.svg.png" alt="php">
+						<h3>PHP</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/html-css.png" alt="css-html">
+						<h3>Html5 & CSS3</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/JS.png" alt="Javascript">
+						<h3>Javascript</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/PhpMyAdmin-Logo.png" alt="PhpMyadmin">
+						<h3>PhpMyAdmin</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/Mysql.svg" alt="MySql">
+						<h3>Mysql</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/git.png" alt="git">
+						<h3>Git</h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/github.png" alt="github">
+						<h3>Github<h3>-->
+					</div>
+	
+					<div>
+						<!--<img src="images/bootstrap.png" alt="bootstrap">
+						<h3>Bootstrap</h3>-->
+					</div>
+	
+					<!--<div>
+						<img src="images/materialize.png" alt="materialize">
+	
+					</div>-->
+					
+					<div>
+						<!--<img src="images/jquery.gif" alt="jquery">
+						<h3>JQuery</h3>-->
+					</div>
+				</section>				
+			</article>
+
+			<!--<article>
+				<div>
+					<h1>Mes Compétences à venir</h1>
+				</div>
+				<div>
+					<img src="images/symfony.png" alt="symfony">
+					<img src="images/angularjs.png" alt="angular">
+					<img src="images/node.png" alt="nodejs">
+					<img src="images/laravel.webp" alt="laravel">
+					<img src="images/Vuejs.png" alt="Vuejs">
+					<img src="images/Python.png" alt="python">
+				</div>
+			</article>-->
+		</section >
+
+		<section id='projet'>
+			<h1 ><span>Pro</span>jets :</h1>
+			<article>
+				<div>
+					<div>
+						<img src="images/reservation-salles.png" alt="reservaton-salles">											
+					</div>
+					<div>
+						<p><span>Pro</span>jet: reservation de salles</p>
+						<p><span>Du</span>rée: 1 semaine</p>
+						<p><span>Lan</span>gages: PHP / HTML / CSS / Proto MVC / POO</p>
+						<p><span>Desc</span>ription: Mise en place d'un planning dynamique.<br>
+						   le planning permet de voir la réservation et le nom de la personne qui réserve la salle<br>
+						   les reservation se font du lundi au vendredi de 8h à 19h
+						</p>
+					</div>
+				</div>
+				<a href="">Voir plus</a>
+			</article>
+			<hr>
+			<article>
+				<div>
+					<div>
+						<img src="images/blog.png" alt="blog">
+					</div>
+					<div>
+						<p><span>Pro</span>jet: Blog </p>
+						<p><span>Du</span>rée: 1 semaine</p>
+						<p><span>Lan</span>gages: PHP / HTML / CSS</p>
+						<p><span>Desc</span>ription: Créer un blog avec un espace administrateur.
+						   Seul l'administrateur  du blog <br> peut créer des articles.
+						   Les articles peuvent être trier par catégorie.
+						   La pagination améliore le dynamisme de l'affichage des articles.<br>
+						   Un système de Like à été ajouté ainsi qu'une wishlist.
+						</p>
+					</div>
+				</div>
+				<a href="">Voir plus</a>
+			</article>	
+			<hr>
+			<article>
+				<div>
+					<div>
+						<img src="images/voyages.png" alt="">
+					</div>
+					<div>
+						<p><span>Pro</span>jet: Blog </p>
+						<p><span>Du</span>rée: 1 semaine</p>
+						<p><span>Lan</span>gages: PHP / HTML / CSS</p>
+						<p><span>Desc</span>ription: Créer un blog avec un espace administrateur.<br>
+						   L'administrateur du blog est le seul à pouvoir créer des articles.
+						   Les articles peuvent être trier par catégorie.<br>
+						   Une améliore le dynamisme de l'affichache des articles
+						   Un système de Like à été ajouté ainsi qu'une wishlist.
+						</p>
+					</div>
+				</div>
+				<a href="">Voir plus</a>
+			</article>
+			<hr>		
+		</section>
+		
+		<section>
+			<h1>Contact	</h1>
+			<input type="text">
+			<textarea name="" id="" cols="50" rows="20"></textarea>
+		</section>
+	</main>
+	<footer>
+		<p></p>
+	</footer>	
+</body>
+</html>
