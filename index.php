cacd3c8e20320aa85bc0b15f44174e0a422be9bd
cyril <cyril.porez@laplateforme.io>
2022-07-07 14:55:10 +0200

    portfolio

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/063b8b7de3.js" crossorigin="anonymous"></script>
 	<title>Cyril Porez</title>
 </head>
 <body>
@@ -30,8 +31,7 @@
 				<p>Porez Cyril</p>
 				<p>Etudiant en devellopement <span>web & mobile</span></p>
 				<a href="#a-propos">A propos</a>
-			</div>
-			
+			</div>			
 		</section>
 
 		<section id="a-propos">
@@ -45,7 +45,7 @@
 				<div>
 					<p>Je m'appelle <span>Cyril Porez</span>, 32ans</p>
 					
-					<p>Diplomé d'un BTS Electronique, A la suite de mes expériences professionnelles, 
+					<p>Technicien en électronique, A la suite de mes expériences professionnelles, 
 					   j'ai décidé de m'orienter vers le développement Web.</p>
 					
 					<p>Je suis passionné par le milieu de la tech dans son ensemble, et j'ai eu la chance 
@@ -56,6 +56,9 @@
 					l'épreuve les compétences acquise au cours de cette première année. Ambitieux et aimant le défi,
 					je serais ravis de mettre à votre disposition mon expérience.</p>
 				</div>
+				<div>
+					<a href="Cyril_Porez.pdf">Télécharger mon CV</a>
+				</div>
 			</article>			
 		</section>
 
@@ -64,73 +67,126 @@
 				<div>
 					<h1><span>Com</span>pétences :</h1>
 				</div>
-
+				<h2>Ce que j'ai appris...</h1>
 				<section>
 					<div>
-						<!--<img src="images/PHP-logo.svg.png" alt="php">
-						<h3>PHP</h3>-->
+						<img src="images/PHP-logo.svg.png" alt="php">
+						<p>PHP</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/html-css.png" alt="css-html">
-						<h3>Html5 & CSS3</h3>-->
+						<img src="images/HTML5.svg" alt="php">
+						<p>HTML5</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/JS.png" alt="Javascript">
-						<h3>Javascript</h3>-->
+						<img src="images/CSS3.svg" alt="php">
+						<p>CSS3</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/PhpMyAdmin-Logo.png" alt="PhpMyadmin">
-						<h3>PhpMyAdmin</h3>-->
+						<img src="images/JavaScript.svg" alt="php">
+						<p>JavaScript</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/Mysql.svg" alt="MySql">
-						<h3>Mysql</h3>-->
+						<img src="images/Jquery.svg" alt="php">
+						<p>Jquery</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/git.png" alt="git">
-						<h3>Git</h3>-->
+						<img src="images/JavaScript.svg" alt="php">
+						<p>MySQL</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/github.png" alt="github">
-						<h3>Github<h3>-->
+						<img src="images/MySQL1.svg" alt="php">
+						<p>MYSQL</p>
 					</div>
 	
 					<div>
-						<!--<img src="images/bootstrap.png" alt="bootstrap">
-						<h3>Bootstrap</h3>-->
+						<img src="images/Wordpress.svg" alt="php">
+						<p>Wordpress</p>
 					</div>
 	
-					<!--<div>
+					<div>
 						<img src="images/materialize.png" alt="materialize">
-	
-					</div>-->
+						<p>Materialize</p>
+					</div>
 					
 					<div>
-						<!--<img src="images/jquery.gif" alt="jquery">
-						<h3>JQuery</h3>-->
+						<img src="images/laravel.webp" alt="jquery">
+						<p>Laravel</p>
 					</div>
-				</section>				
+					<div>
+						<img src="images/Vuejs.png" alt="jquery">
+						<p>Vuejs</p>
+					</div>
+					<div>
+						<img src="images/node.png" alt="jquery">
+						<p>Nodejs</p>
+					</div>
+					<div>
+						<img src="images/docker-logo.png" alt="jquery">
+						<p>Docker</p>
+					</div>
+					<div>
+						<img src="images/Git.png" alt="jquery">
+						<p>Docker</p>
+					</div>
+				</section>
+				<h2>Ce que je vais apprendre...</h2>
+				<section>
+					<div>
+						<img src="images/Python.svg" alt="materialize">
+						<p>Python</p>
+					</div>
+					
+					<div>
+						<img src="images/Slim.png" alt="jquery">
+						<p>Slim</p>
+					</div>
+					<div>
+						<img src="images/Sass.svg" alt="jquery">
+						<p>Sass</p>
+					</div>
+					<div>
+						<img src="images/Symfony.png" alt="jquery">
+						<p>Symfony</p>
+					</div>
+					<div>
+						<img src="images/React.svg" alt="jquery">
+						<p>React</p>
+					</div>
+				</section>
+				<h2>les outils que j'utilise...</h2>
+				<section>					
+					<div>
+						<img src="images/VSCode.svg" alt="jquery">
+						<p>VsCode</p>
+					</div>
+					<div>
+						<img src="images/Github.svg" alt="jquery">
+						<p>Github</p>
+					</div>
+					<div>
+						<img src="images/Composer.svg" alt="jquery">
+						<p>Composer</p>
+					</div>
+					<div>
+						<img src="images/Npm.svg" alt="jquery">
+						<p>Npm</p>
+					</div>
+					<div>
+						<img src="images/Figma.svg" alt="jquery">
+						<p>Figma</p>
+					</div>
+					<div>
+						<img src="images/Trello.svg" alt="jquery">
+						<p>Trello</p>
+					</div>
+				</section>			
 			</article>
-
-			<!--<article>
-				<div>
-					<h1>Mes Compétences à venir</h1>
-				</div>
-				<div>
-					<img src="images/symfony.png" alt="symfony">
-					<img src="images/angularjs.png" alt="angular">
-					<img src="images/node.png" alt="nodejs">
-					<img src="images/laravel.webp" alt="laravel">
-					<img src="images/Vuejs.png" alt="Vuejs">
-					<img src="images/Python.png" alt="python">
-				</div>
-			</article>-->
 		</section >
 
 		<section id='projet'>
@@ -138,7 +194,7 @@
 			<article>
 				<div>
 					<div>
-						<img src="images/reservation-salles.png" alt="reservaton-salles">											
+						<img src="images/reservation-salles.png" alt="reservation-salles">											
 					</div>
 					<div>
 						<p><span>Pro</span>jet: reservation de salles</p>
@@ -150,7 +206,7 @@
 						</p>
 					</div>
 				</div>
-				<a href="">Voir plus</a>
+				<a href="https://cyril-porez.students-laplateforme.io/reservation-salles">Voir plus</a>
 			</article>
 			<hr>
 			<article>
@@ -170,7 +226,7 @@
 						</p>
 					</div>
 				</div>
-				<a href="">Voir plus</a>
+				<a href="https://cyril-porez.students-laplateforme.io/blog">Voir plus</a>
 			</article>	
 			<hr>
 			<article>
@@ -190,19 +246,27 @@
 						</p>
 					</div>
 				</div>
-				<a href="">Voir plus</a>
+				<!--<a href="https://cyril-porez.students-laplateforme.io/voyages">Voir plus</a>-->
+			</article>		
+		</section>		
+		<section>			
+			<h1><span>Con</span>tact</h1>
+			<article>
+				<p>Mon profil vous intéresse, vous souhaitez collaborer avec moi, n'hésiter pas à me Contacter !</p>
+				<div>
+					<a href="mailto:cyril.porez@laplateforme.io">Me contacter</a>
+				</div>
+				<p>Mes réseaux sociaux</p>
+				<div>
+					<a href="https://www.linkedin.com/in/cyril-porez-73a551231/"><img src="images/icons8-linkedin.svg" alt="linkedin"></a> 
+					<a href="https://github.com/cyril-porez"><img src="images/icons8-github.svg" alt="github"></a>
+				</div>
 			</article>
-			<hr>		
-		</section>
-		
-		<section>
-			<h1>Contact	</h1>
-			<input type="text">
-			<textarea name="" id="" cols="50" rows="20"></textarea>
 		</section>
 	</main>
 	<footer>
-		<p></p>
+		<p> © Cyril-Porez</p>
+		
 	</footer>	
 </body>
 </html>
