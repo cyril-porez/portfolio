b92f22ae5f393260c300720ce7f6985c1f369e97
cyril <cyril.porez@laplateforme.io>
2022-11-15 14:36:13 +0100

    travaille sur le responsive

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="portfolio/desktop.css">
+	<link rel="stylesheet" href="portfolio/mobile-first.css">
 	<script src="https://kit.fontawesome.com/063b8b7de3.js" crossorigin="anonymous"></script>
 	<title>Cyril Porez</title>
 </head>
@@ -12,7 +13,7 @@
 	<header>
 		<nav>
 			<div>
-				<a href="#">Porez Cyril</a>
+				<a href="#home">Porez Cyril</a>
 				<ul>
 					<a href="#a-propos"><li>About</li></a>
 					<a href="#competences"><li>Skills</li></a>
@@ -24,24 +25,24 @@
 	</header>
 	
 	<main>
-		<section>
-			<img src="images/portofiio_background_img.png" alt="image d'accueil">
+		<section id='home'>
+			<img src="portfolio/images/portofiio_background_img.png" alt="image d'accueil">
 			<div>
 				<p>Bonjour, je suis</p>
 				<p>Porez Cyril</p>
 				<p>Etudiant en devellopement <span>web & mobile</span></p>
 				<a href="#a-propos">A propos</a>
-			</div>			
+			</div>
+			
 		</section>
 
 		<section id="a-propos">
 			
-			<img src="images/Jotaro.png" alt="ken">
+			<img src="portfolio/images/firmware2.jpg" alt="image">
 			<article>
 				<div>
 					<h1><span>A</span> propos :</h1>
-				</div>
-				
+				</div>				
 				<div>
 					<p>Je m'appelle <span>Cyril Porez</span>, 32ans</p>
 					
@@ -57,7 +58,7 @@
 					je serais ravis de mettre à votre disposition mon expérience.</p>
 				</div>
 				<div>
-					<a href="Cyril_Porez.pdf">Télécharger mon CV</a>
+					<a href="portfolio/Cyril Porez.pdf">Télécharger mon CV</a>
 				</div>
 			</article>			
 		</section>
@@ -70,119 +71,114 @@
 				<h2>Ce que j'ai appris...</h1>
 				<section>
 					<div>
-						<img src="images/PHP-logo.svg.png" alt="php">
+						<img src="portfolio/images/PHP-logo.svg.png" alt="php">
 						<p>PHP</p>
 					</div>
 	
 					<div>
-						<img src="images/HTML5.svg" alt="php">
+						<img src="portfolio/images/HTML5.svg" alt="php">
 						<p>HTML5</p>
 					</div>
 	
 					<div>
-						<img src="images/CSS3.svg" alt="php">
+						<img src="portfolio/images/CSS3.svg" alt="php">
 						<p>CSS3</p>
 					</div>
 	
 					<div>
-						<img src="images/JavaScript.svg" alt="php">
+						<img src="portfolio/images/JavaScript.svg" alt="php">
 						<p>JavaScript</p>
 					</div>
 	
 					<div>
-						<img src="images/Jquery.svg" alt="php">
+						<img src="portfolio/images/Jquery.svg" alt="php">
 						<p>Jquery</p>
 					</div>
 	
 					<div>
-						<img src="images/JavaScript.svg" alt="php">
-						<p>MySQL</p>
-					</div>
-	
-					<div>
-						<img src="images/MySQL1.svg" alt="php">
+						<img src="portfolio/images/MySQL1.svg" alt="php">
 						<p>MYSQL</p>
 					</div>
 	
 					<div>
-						<img src="images/Wordpress.svg" alt="php">
+						<img src="portfolio/images/Wordpress.svg" alt="php">
 						<p>Wordpress</p>
 					</div>
 	
 					<div>
-						<img src="images/materialize.png" alt="materialize">
+						<img src="portfolio/images/materialize.png" alt="materialize">
 						<p>Materialize</p>
 					</div>
 					
 					<div>
-						<img src="images/laravel.webp" alt="jquery">
+						<img src="portfolio/images/laravel.webp" alt="Laravel">
 						<p>Laravel</p>
 					</div>
 					<div>
-						<img src="images/Vuejs.png" alt="jquery">
+						<img src="portfolio/images/Vuejs.png" alt="Vuejs">
 						<p>Vuejs</p>
 					</div>
 					<div>
-						<img src="images/node.png" alt="jquery">
+						<img src="portfolio/images/node.png" alt="nodejs">
 						<p>Nodejs</p>
 					</div>
 					<div>
-						<img src="images/docker-logo.png" alt="jquery">
+						<img src="portfolio/images/docker-logo.png" alt="Docker">
 						<p>Docker</p>
 					</div>
 					<div>
-						<img src="images/Git.png" alt="jquery">
-						<p>Docker</p>
+						<img src="portfolio/images/Git.svg" alt="GIT">
+						<p>GIT</p>
 					</div>
 				</section>
 				<h2>Ce que je vais apprendre...</h2>
 				<section>
 					<div>
-						<img src="images/Python.svg" alt="materialize">
+						<img src="portfolio/images/Python.svg" alt="materialize">
 						<p>Python</p>
 					</div>
 					
 					<div>
-						<img src="images/Slim.png" alt="jquery">
+						<img src="portfolio/images/Slim.png" alt="slim">
 						<p>Slim</p>
 					</div>
 					<div>
-						<img src="images/Sass.svg" alt="jquery">
+						<img src="portfolio/images/Sass.svg" alt="SASS">
 						<p>Sass</p>
 					</div>
 					<div>
-						<img src="images/Symfony.png" alt="jquery">
+						<img src="portfolio/images/Symfony.svg" alt="Symfony">
 						<p>Symfony</p>
 					</div>
 					<div>
-						<img src="images/React.svg" alt="jquery">
+						<img src="portfolio/images/React.svg" alt="React">
 						<p>React</p>
 					</div>
 				</section>
 				<h2>les outils que j'utilise...</h2>
 				<section>					
 					<div>
-						<img src="images/VSCode.svg" alt="jquery">
+						<img src="portfolio/images/VSCode.svg" alt="Vscode">
 						<p>VsCode</p>
 					</div>
 					<div>
-						<img src="images/Github.svg" alt="jquery">
+						<img src="portfolio/images/Github.svg" alt="Github">
 						<p>Github</p>
 					</div>
 					<div>
-						<img src="images/Composer.svg" alt="jquery">
+						<img src="portfolio/images/Composer.svg" alt="Composer">
 						<p>Composer</p>
 					</div>
 					<div>
-						<img src="images/Npm.svg" alt="jquery">
+						<img src="portfolio/images/Npm.svg" alt="Npm">
 						<p>Npm</p>
 					</div>
 					<div>
-						<img src="images/Figma.svg" alt="jquery">
+						<img src="portfolio/images/Figma.svg" alt="Figma">
 						<p>Figma</p>
 					</div>
 					<div>
-						<img src="images/Trello.svg" alt="jquery">
+						<img src="portfolio/images/Trello.svg" alt="Trello">
 						<p>Trello</p>
 					</div>
 				</section>			
@@ -194,7 +190,43 @@
 			<article>
 				<div>
 					<div>
-						<img src="images/reservation-salles.png" alt="reservation-salles">											
+						<img class="app" src="portfolio/images/lilith.jpg" alt="lilith">										
+					</div>
+					<div>
+						<p><span>Pro</span>jet: Lilith Web App</p>
+						<p><span>Du</span>rée: 3 mois</p>
+						<p><span>Lan</span>gages: ReactJs, Strapi, TailwindCSS, Docker</p>
+						<p><span>Desc</span>ription: Projet pensé par des élèves de l'innovation Lab au sein de La plateforme.<br>
+							Lilith consiste à mettre en relation des féministes afin de favoriser leurs actions. Pour ce faire, elles 									bénficient grâce à l'applcation de création de sondage, Création de Channel, Channel de discussion.  
+						</p>
+					</div>
+				</div>
+				<a href="https://lilith.new-atelier.athomas.io/">Voir plus</a>
+			</article>
+			<hr>
+			<article>
+				<div>
+					<div>
+						<img class="web" src="portfolio/images/Canage.png" alt="Carnage">										
+					</div>
+					<div>
+						<p><span>Pro</span>jet: Boutique en Ligne</p>
+						<p><span>Du</span>rée: 3 Semaines</p>
+						<p><span>Lan</span>gages: PHP / HTML / CSS / MVC / POO</p>
+						<p><span>Desc</span>ription: Mise en place d'une boutique e-commerce<br>
+							Possibilité de faire une recherche de produit. D'acheter un produit.
+							Un back-office permettant à l'administrateur de géréer le site
+						</p>
+						<p class="Warning">Attention site en Maintenance nombreuses Erreurs possibles !!</p>
+					</div>
+				</div>
+				<a href="">Voir plus</a>
+			</article>
+			<hr>
+			<article>
+				<div>
+					<div>
+						<img class="web" src="portfolio/images/reservation-salles.png" alt="reservation-salles">											
 					</div>
 					<div>
 						<p><span>Pro</span>jet: reservation de salles</p>
@@ -212,18 +244,14 @@
 			<article>
 				<div>
 					<div>
-						<img src="images/blog.png" alt="blog">
+						<img class="web" src="portfolio/images/blog.png" alt="blog">
 					</div>
 					<div>
-						<p><span>Pro</span>jet: Blog </p>
+						<p><span>Pro</span>jet: Voyages </p>
 						<p><span>Du</span>rée: 1 semaine</p>
-						<p><span>Lan</span>gages: PHP / HTML / CSS</p>
-						<p><span>Desc</span>ription: Créer un blog avec un espace administrateur.
-						   Seul l'administrateur  du blog <br> peut créer des articles.
-						   Les articles peuvent être trier par catégorie.
-						   La pagination améliore le dynamisme de l'affichage des articles.<br>
-						   Un système de Like à été ajouté ainsi qu'une wishlist.
-						</p>
+						<p><span>Lan</span>gages: HTML / CSS</p>
+						<p><span>Desc</span>ription: Site vitrine développé en
+						equipe de 3 en utilisant seulement HTML/CSS.</p>
 					</div>
 				</div>
 				<a href="https://cyril-porez.students-laplateforme.io/blog">Voir plus</a>
@@ -232,7 +260,7 @@
 			<article>
 				<div>
 					<div>
-						<img src="images/voyages.png" alt="">
+						<img class="web" src="portfolio/images/voyages.png" alt="">
 					</div>
 					<div>
 						<p><span>Pro</span>jet: Blog </p>
@@ -246,10 +274,11 @@
 						</p>
 					</div>
 				</div>
-				<!--<a href="https://cyril-porez.students-laplateforme.io/voyages">Voir plus</a>-->
+				<a href="https://cyril-porez.students-laplateforme.io/voyages">Voir plus</a>
+
 			</article>		
 		</section>		
-		<section>			
+		<section id="contact">			
 			<h1><span>Con</span>tact</h1>
 			<article>
 				<p>Mon profil vous intéresse, vous souhaitez collaborer avec moi, n'hésiter pas à me Contacter !</p>
@@ -258,15 +287,14 @@
 				</div>
 				<p>Mes réseaux sociaux</p>
 				<div>
-					<a href="https://www.linkedin.com/in/cyril-porez-73a551231/"><img src="images/icons8-linkedin.svg" alt="linkedin"></a> 
-					<a href="https://github.com/cyril-porez"><img src="images/icons8-github.svg" alt="github"></a>
+					<a href="https://www.linkedin.com/in/cyril-porez-73a551231/"><img src="portfolio/images/icons8-linkedin.svg" alt="linkedin"></a> 
+					<a href="https://github.com/cyril-porez"><img src="portfolio/images/icons8-github.svg" alt="github"></a>
 				</div>
 			</article>
 		</section>
 	</main>
 	<footer>
-		<p> © Cyril-Porez</p>
-		
+		<p>copyright © Cyril-Porez</p>		
 	</footer>	
 </body>
 </html>
